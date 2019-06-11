e5657aa677754c63e6aa553c787377bf18a42f0d
lisi <598926425@qq.com>
2019-06-11 16:36:21 +0800

    123

---
+++ demo1/src/com/itheima/demo01.java
@@ -0,0 +1,10 @@
+package com.itheima;
+
+public class demo01 {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+    }
+}
